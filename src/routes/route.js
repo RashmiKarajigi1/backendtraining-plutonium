08793454063d4fd4049c00abac12530eb720eb68
RashmiKarajigi1 <rashmimkarajigi@gmail.com>
2022-08-08 12:58:48 +0530

    my lodash assignments

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash=require('lodash');
 const abc = require('../introduction/intro')
 const exportModule = require('../logger/logger')
 const helpe = require('../util/helper')
@@ -11,13 +12,20 @@ const router = express.Router();
 //     res.send('My second ever api!')
 // });
 router.get('/test-me',function(req,res){
-    exportModule.welcome()
-    helpe.printDate()
-    helpe.printMonth()
-    helpe.getBatchInfo()
-    format.trim()
-    format.changetoLowerCase()
-    format.changeToUpperCase()
+    const arrayMonth=['January','February','March','April','May','June','July','August','September','October','November','December'];
+    console.log(lodash.chunk(arrayMonth,3))
+    const arrayOddNumbers = [1,3,5,7,9,11,13,15,17,19]
+    console.log(lodash.tail(arrayOddNumbers))
+    const array1=[1,2]
+    const array2=[2,3]
+    const array3=[3,4]
+    const array4=[4,5]
+    const array5=[5,6]
+     let x = lodash.union(array1,array2,array3,array4,array5)
+     console.log(x)
+     const arrayMovies = [["Horror","The Shining"],["Drama","Titanic"],["Thriller","Shutter Island"],["Fantasy","Pans Labyrinth"]]
+    console.log(lodash.fromPairs(arrayMovies))
+
     res.send('3rd API')
 });
 
