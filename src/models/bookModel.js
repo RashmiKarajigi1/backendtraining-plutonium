2c347e6bad54dbe63a4f3faf0c303ea2a4f9cf40
RashmiKarajigi1 <rashmimkarajigi@gmail.com>
2022-08-16 13:30:23 +0530

    mongoose

---
+++ src/models/bookModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    bookName:{ 
+        type : String,
+        required : true,
+    },
+    authorName: String,
+    category: {
+        type: String,
+        enum: ["Love story", "Suspense", "Romantic", "Finance", "Trading", "Development", "Science","Others"]
+    },
+    year: Number,
+}, { timestamps: true });
+
+module.exports = mongoose.model('Book', bookSchema) 
