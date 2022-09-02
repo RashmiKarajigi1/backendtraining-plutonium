0255be1665746482cf9c2dd939531ae696ae9258
RashmiKarajigi1 <rashmimkarajigi@gmail.com>
2022-09-02 22:55:54 +0530

    bjbbc

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,24 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const userController = require("../controllers/userController")
-const commonMiddleware = require("../commonMiddleware/auth")
+
+const memeController = require("../controllers/memeController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-//API to createUser
-router.post("/users", userController.createUser)
 
-//API to Login User
-router.post("/login", userController.loginUser)
 
-//The userId is sent by front end
-router.get("/users/:userId", commonMiddleware.authenticate,commonMiddleware.authorise,userController.getUserData)
+router.get("/memes/getAllMemes", memeController.getAllMemes)
+router.post("/memes/memeEditor", memeController.memeEditor)
 
-router.post("/users/:userId/posts", commonMiddleware.authenticate,commonMiddleware.authorise,userController.postMessage)
 
-router.put("/users/:userId", commonMiddleware.authenticate,commonMiddleware.authorise, userController.updateUser)
 
-router.delete("/users/:userId", commonMiddleware.authenticate,commonMiddleware.authorise, userController.deleteUser)
 
 module.exports = router;
